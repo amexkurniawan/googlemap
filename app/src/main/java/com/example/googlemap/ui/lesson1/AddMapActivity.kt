ae7a6c1b6b366db75fe8d905415ece72c6265a6c
rahmat.azwar <rahmat.azwar@iconpln.co.id>
2021-06-21 15:35:56 +0700

    [RK] - Lesson1 - add map to activity and set marker

--- app/src/main/java/com/example/googlemap/ui/lesson1/AddMapActivity.kt
+++ app/src/main/java/com/example/googlemap/ui/lesson1/AddMapActivity.kt
@@ -3,10 +3,40 @@ package com.example.googlemap.ui.lesson1
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.googlemap.R
+import com.example.googlemap.utils.place.Place
+import com.example.googlemap.utils.place.PlacesReader
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.MarkerOptions
 
 class AddMapActivity : AppCompatActivity() {
+
+    private val places: List<Place> by lazy {
+        PlacesReader(this).read()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_map)
+
+        val mapFragment = supportFragmentManager.findFragmentById(
+            R.id.map_fragment
+        ) as? SupportMapFragment
+        mapFragment?.getMapAsync { googleMap ->
+            addMarkers(googleMap)
+        }
+    }
+
+    /**
+     * Adds marker representations of the places list on the provided GoogleMap object
+     */
+    private fun addMarkers(googleMap: GoogleMap) {
+        places.forEach { place ->
+            val marker = googleMap.addMarker(
+                MarkerOptions()
+                    .title(place.name)
+                    .position(place.latLng)
+            )
+        }
     }
 }
