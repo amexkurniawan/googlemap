19af06c068ba1c9150be5f19cb86fc912ba30696
rahmat.azwar <rahmat.azwar@iconpln.co.id>
2021-06-21 15:33:14 +0700

    [RK] - Helper - add place helper

---
+++ app/src/main/java/com/example/googlemap/utils/place/Place.kt
@@ -0,0 +1,10 @@
+package com.example.googlemap.utils.place
+
+import com.google.android.gms.maps.model.LatLng
+
+data class Place(
+    val name: String,
+    val latLng: LatLng,
+    val address: String,
+    val rating: Float
+)
