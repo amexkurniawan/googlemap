562c62e0205ddf94cc78638d74765c937185baa6
rahmat.azwar <rahmat.azwar@iconpln.co.id>
2021-06-21 15:27:37 +0700

    [RK] - Main - add menu and add activity

--- app/src/main/java/com/example/googlemap/MainActivity.kt
+++ app/src/main/java/com/example/googlemap/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.googlemap
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.example.googlemap.ui.lesson1.AddMapActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        onClickAction()
+    }
+
+    private fun onClickAction() {
+        findViewById<Button>(R.id.btnGMap1).setOnClickListener {
+            val intent = Intent(this, AddMapActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/example/googlemap/ui/lesson1/AddMapActivity.kt
@@ -0,0 +1,12 @@
+package com.example.googlemap.ui.lesson1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.googlemap.R
+
+class AddMapActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_map)
+    }
+}
