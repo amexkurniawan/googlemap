33dff5b4a02957d1e2b79d35f94b1732ea753301
rahmat.azwar <rahmat.azwar@iconpln.co.id>
2021-06-22 10:48:39 +0700

    [RK] - Lesson2 - set permission

--- app/src/main/java/com/example/googlemap/ar/PlacesArFragment.kt
+++ app/src/main/java/com/example/googlemap/ar/PlacesArFragment.kt
@@ -6,6 +6,6 @@ import com.google.ar.sceneform.ux.ArFragment
 class PlacesArFragment : ArFragment() {
 
     override fun getAdditionalPermissions(): Array<String> =
-        // TODO return location permission
-        emptyArray()
+        listOf(Manifest.permission.ACCESS_FINE_LOCATION)
+            .toTypedArray()
 }
