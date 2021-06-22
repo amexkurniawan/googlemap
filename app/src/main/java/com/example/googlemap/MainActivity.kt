3a04df80c8b28136bb48c29e4c0be4130942d054
rahmat.azwar <rahmat.azwar@iconpln.co.id>
2021-06-22 09:13:37 +0700

    [RK] - Lesson1 - add map ktx activity

--- app/src/main/java/com/example/googlemap/MainActivity.kt
+++ app/src/main/java/com/example/googlemap/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import com.example.googlemap.ui.lesson1.AddMapActivity
+import com.example.googlemap.ui.lesson1.AddMapKtxActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,6 +18,10 @@ class MainActivity : AppCompatActivity() {
     private fun onClickAction() {
         findViewById<Button>(R.id.btnGMap1).setOnClickListener {
             val intent = Intent(this, AddMapActivity::class.java)
+            startActivity(intent)
+        }
+        findViewById<Button>(R.id.btnGMap2).setOnClickListener {
+            val intent = Intent(this, AddMapKtxActivity::class.java)
             startActivity(intent)
         }
     }
