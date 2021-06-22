eb6e4fa8b386a583baaa01bfb0fbdfaed4734039
rahmat.azwar <rahmat.azwar@iconpln.co.id>
2021-06-22 12:58:13 +0700

    [RK] - Lesson3 - set polygon to map

--- app/src/main/java/com/example/googlemap/ui/lesson3/PolylinePolygonActivity.kt
+++ app/src/main/java/com/example/googlemap/ui/lesson3/PolylinePolygonActivity.kt
@@ -15,14 +15,28 @@ class PolylinePolygonActivity : AppCompatActivity(),
     GoogleMap.OnPolylineClickListener,
     GoogleMap.OnPolygonClickListener {
 
+    // Polyline
     private val COLOR_BLACK_ARGB = -0x1000000
     private val POLYLINE_STROKE_WIDTH_PX = 12
     private val PATTERN_GAP_LENGTH_PX = 20
     private val DOT: PatternItem = Dot()
     private val GAP: PatternItem = Gap(PATTERN_GAP_LENGTH_PX.toFloat())
 
+    // Polygon
+    private val COLOR_WHITE_ARGB = -0x1
+    private val COLOR_GREEN_ARGB = -0xc771c4
+    private val COLOR_PURPLE_ARGB = -0x7e387c
+    private val COLOR_ORANGE_ARGB = -0xa80e9
+    private val COLOR_BLUE_ARGB = -0x657db
+    private val POLYGON_STROKE_WIDTH_PX = 8
+    private val PATTERN_DASH_LENGTH_PX = 20
+    private val DASH: PatternItem = Dash(PATTERN_DASH_LENGTH_PX.toFloat())
+    private val PATTERN_POLYGON_ALPHA = listOf(GAP, DASH)
+
     // Create a stroke pattern of a gap followed by a dot.
     private val PATTERN_POLYLINE_DOTTED = listOf(GAP, DOT)
+    // Create a stroke pattern of a dot followed by a gap, a dash, and another gap.
+    private val PATTERN_POLYGON_BETA = listOf(DOT, GAP, DASH, GAP)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -59,6 +73,36 @@ class PolylinePolygonActivity : AppCompatActivity(),
     }
 
     /**
+     * Styles the polygon, based on type.
+     * @param polygon The polygon object that needs styling.
+     */
+    private fun stylePolygon(polygon: Polygon) {
+        // Get the data object stored with the polygon.
+        val type = polygon.tag?.toString() ?: ""
+        var pattern: List<PatternItem>? = null
+        var strokeColor = COLOR_BLACK_ARGB
+        var fillColor = COLOR_WHITE_ARGB
+        when (type) {
+            "alpha" -> {
+                // Apply a stroke pattern to render a dashed line, and define colors.
+                pattern = PATTERN_POLYGON_ALPHA
+                strokeColor = COLOR_GREEN_ARGB
+                fillColor = COLOR_PURPLE_ARGB
+            }
+            "beta" -> {
+                // Apply a stroke pattern to render a line of dots and dashes, and define colors.
+                pattern = PATTERN_POLYGON_BETA
+                strokeColor = COLOR_ORANGE_ARGB
+                fillColor = COLOR_BLUE_ARGB
+            }
+        }
+        polygon.strokePattern = pattern
+        polygon.strokeWidth = POLYGON_STROKE_WIDTH_PX.toFloat()
+        polygon.strokeColor = strokeColor
+        polygon.fillColor = fillColor
+    }
+
+    /**
      * Manipulates the map when it's available.
      * The API invokes this callback when the map is ready to be used.
      * This is where we can add markers or lines, add listeners or move the camera.
@@ -79,6 +123,21 @@ class PolylinePolygonActivity : AppCompatActivity(),
                 LatLng(-32.491, 147.309)))
         // Store a data object with the polyline, used here to indicate an arbitrary type.
         polyline1.tag = "A"
+        // Style the polyline
+        stylePolyline(polyline1)
+
+        // Add polygons to indicate areas on the map.
+        val polygon1 = googleMap.addPolygon(PolygonOptions()
+            .clickable(true)
+            .add(
+                LatLng(-27.457, 153.040),
+                LatLng(-33.852, 151.211),
+                LatLng(-37.813, 144.962),
+                LatLng(-34.928, 138.599)))
+        // Store a data object with the polygon, used here to indicate an arbitrary type.
+        polygon1.tag = "alpha"
+        // Style the polygon.
+        stylePolygon(polygon1)
 
         // Position the map's camera near Alice Springs in the center of Australia,
         // and set the zoom factor so most of Australia shows on the screen.
