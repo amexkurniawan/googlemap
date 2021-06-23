68a838b592f02e7b0a9485c4613252eb66ca861d
rahmat.azwar <rahmat.azwar@iconpln.co.id>
2021-06-23 16:12:10 +0700

    [RK] - AR - set up google map

--- app/src/main/java/com/example/googlemap/ui/asset_register/AssetRegisterActivity.kt
+++ app/src/main/java/com/example/googlemap/ui/asset_register/AssetRegisterActivity.kt
@@ -3,10 +3,25 @@ package com.example.googlemap.ui.asset_register
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.googlemap.R
+import com.google.android.gms.maps.SupportMapFragment
 
 class AssetRegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_asset_register)
+
+        setUpMap()
+    }
+
+    /**
+     * reference to the GoogleMap object
+     */
+    private fun setUpMap() {
+        val mapFragment = supportFragmentManager.findFragmentById(
+            R.id.asset_register_map
+        ) as? SupportMapFragment
+        mapFragment?.getMapAsync { googleMap ->
+            // set mark
+        }
     }
 }
