b9ac10170440f751cf83fa313af69bdaca14f716
rahmat.azwar <rahmat.azwar@iconpln.co.id>
2021-06-24 10:13:25 +0700

    [RK] - AR - set my location

--- app/src/main/java/com/example/googlemap/ui/asset_register/AssetRegisterActivity.kt
+++ app/src/main/java/com/example/googlemap/ui/asset_register/AssetRegisterActivity.kt
@@ -1,17 +1,23 @@
 package com.example.googlemap.ui.asset_register
 
+import android.annotation.SuppressLint
+import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.core.content.ContextCompat
 import com.example.googlemap.R
+import com.example.googlemap.ui.lesson2.latLng
 import com.example.googlemap.utils.asset_register.Asset
 import com.example.googlemap.utils.asset_register.AssetReader
 import com.example.googlemap.utils.helper.BitmapHelper
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.BitmapDescriptor
-import com.google.android.gms.maps.model.LatLngBounds
+import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.MarkerOptions
 
 class AssetRegisterActivity : AppCompatActivity() {
@@ -25,6 +31,10 @@ class AssetRegisterActivity : AppCompatActivity() {
         BitmapHelper.vectorToBitmap(this, R.drawable.ic_baseline_place_24, color)
     }
 
+    private val TAG = "AssetRegisterActivity"
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private var currentLocation: Location? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_asset_register)
@@ -35,21 +45,23 @@ class AssetRegisterActivity : AppCompatActivity() {
     /**
      * reference to the GoogleMap object
      */
+    @SuppressLint("MissingPermission")
     private fun setUpMap() {
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
         val mapFragment = supportFragmentManager.findFragmentById(
             R.id.asset_register_map
         ) as? SupportMapFragment
         mapFragment?.getMapAsync { googleMap ->
-            // set camera focus
-            googleMap.setOnMapLoadedCallback {
-                val bounds = LatLngBounds.builder()
-                asset.forEach { bounds.include(it.latLng) }
-                googleMap.moveCamera(CameraUpdateFactory.newLatLngBounds(bounds.build(), 18))
-            }
+            googleMap.isMyLocationEnabled = true
             // Info Window Content
             googleMap.setInfoWindowAdapter(MarkerAssetInfoAdapter(this))
-            // set mark
-            setMarkers(googleMap)
+            // get current location & set camera focus
+            getCurrentLocation {
+                val pos = CameraPosition.fromLatLngZoom(it.latLng, 13f)
+                googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(pos))
+                setMarkers(googleMap)
+            }
         }
     }
 
@@ -65,6 +77,16 @@ class AssetRegisterActivity : AppCompatActivity() {
                     .icon(assetIcon)
             )
             marker.tag = data
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun getCurrentLocation(onSuccess: (Location) -> Unit) {
+        fusedLocationClient.lastLocation.addOnSuccessListener { location ->
+            currentLocation = location
+            onSuccess(location)
+        }.addOnFailureListener {
+            Log.e(TAG, "Could not get location")
         }
     }
 }
